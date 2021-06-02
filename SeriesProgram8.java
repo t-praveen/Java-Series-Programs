5102f54ecb42d4ae1294d75cd60887aa869f348b
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-06-02 15:51:45 +0530

    Add files via upload

---
+++ SeriesProgram8.java
@@ -0,0 +1,21 @@
+//Triangular Number Series //
+
+package seriesprograms;
+import java.util.Scanner ;
+
+public class SeriesProgram8 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Limit : ");
+		int n = sc.nextInt();
+		for(int i = 1 ; i <= n ; i++)
+		{
+			int num = (i * ( i + 1) / 2 ) ;
+			System.out.print(" "+num);
+		}
+		sc.close();
+	}
+
+}
