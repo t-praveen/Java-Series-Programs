f563894b05b567340423e747004ab6640781fb7c
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-06-02 15:51:06 +0530

    Add files via upload

---
+++ SeriesProgram5.java
@@ -0,0 +1,20 @@
+//Square Number Series //
+
+package seriesprograms;
+import java.util.Scanner ;
+
+public class SeriesProgram5 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Limit : ");
+		int n = sc.nextInt();
+		for(int i = 1 ; i <= n ; i++)
+		{
+			System.out.print(" "+(i*i));
+		}
+		sc.close();
+	}
+
+}
